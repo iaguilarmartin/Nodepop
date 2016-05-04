f3c2281b2171e7ecbbdd8ba67915e33010fc3bee
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-05-04 17:01:48 +0200

    Se añade la funcionalidad de autentificar usuarios usando el modulo de jsonwebtokens

--- app_config.js
+++ app_config.js
@@ -4,6 +4,12 @@ module.exports = {
     dbURL: 'mongodb://localhost:27017/nodepop',
     initialDataScript: 'initialData.json',
     errorsFile: 'errorMessages.json',
+    jwt: {
+        secret: "fdsgdfgtrerhgnhfgjyrturty"
+    },
+    crypto: {
+        secret: "aweerttrhghfhjghj"
+    },
     tags: [
         'lifestyle',
         'motor',
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,7 +2,9 @@
 
 var encrypt = require('../../../lib/encrypt');
 var sendError = require('../../../lib/customErrors');
+var config = require('../../../app_config');
 
+var jwt = require('jsonwebtoken');
 var express = require('express');
 var router = express.Router();
 var User = require('mongoose').model('User');
@@ -34,6 +36,36 @@ router.post('/', function (req, res, next) {
         }
 
         res.json({success: true, result: saved});
+    });
+});
+
+router.post("/authenticate", function(req, res, next) {
+    var mail = req.body.mail;
+    var pass = req.body.pass;
+
+    if (!mail) {
+        return sendError('AUTH_MAIL_NOT_FOUND', req, res, 401);
+    }
+    if (!pass) {
+        return sendError('AUTH_INVALID_PASS', req, res, 401);
+    }
+
+    User.findOne({mail:mail}).exec(function (err, user) {
+        if (err) {
+            return res.status(500).json({success: false, error: err});
+        }
+
+        if (!user) {
+            return sendError('AUTH_MAIL_NOT_FOUND', req, res, 401);
+        }
+
+        if (user.pass !== encrypt(pass)) {
+            return sendError('AUTH_INVALID_PASS', req, res, 401);
+        }
+
+        var token = jwt.sign({ id: user._id}, config.jwt.secret, { expiresIn: '7 days' });
+
+        res.json({success: true, token: token});
     });
 });
 
